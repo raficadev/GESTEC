6a1c0a37ee33fec14d5176aaac4c0fcb41b6941b
Rafel Castelló Fiol <133007264+raficadev@users.noreply.github.com>
2024-05-22 19:05:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ setup(
     version="1.0.0",
     author="Rafel Castelló Fiol",
     author_email="raficadev@raficadev.onmicrosoft.com",
-    description="Gestor de tareas de consola GESTEC",
+    description="Gestor de tareas de Terminal/Consola",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/raficadev/GESTEC",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)",
         "Operating System :: OS Independent",
     ],
     install_requires=[
         "colorama",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     entry_points={
         'console_scripts': [
             'gestec=gestec:main',
